3738f6ef7d290ec47a29693b47c970fe362bd57b
xkamikoo <58092037+xkamikoo@users.noreply.github.com>
2023-08-16 10:04:19 +0200

    add CSS variables to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      white: "var(--white)",
+      beige: "var(--beige)",
+      black: "var(--black)",
+      green: "var(--green)",
+      grey: "var(--grey)",
+    },
+    fontFamily: {
+      sans: "var(--inter)",
+      heading: "var(--montserrat)",
+    }
+    
   },
   plugins: [],
 }
