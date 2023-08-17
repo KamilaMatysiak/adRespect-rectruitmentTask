4c453c0aa50cf213e8b0e6d15c8fa0ea7d6fce2c
xkamikoo <58092037+xkamikoo@users.noreply.github.com>
2023-08-17 13:45:53 +0200

    Masonry with view more

--- main.js
+++ main.js
@@ -1,2 +1,33 @@
-//testing if script is working
-document.querySelector('#banner').innerHTML = `Banner`
+var masonry = new Macy({
+    container: '#macy-container',
+    mobileFirst: true,
+    trueOrder: false,
+    waitForImages: false,
+    columns: 1,
+    margin: 43,
+    breakAt: {
+        1600: 4,
+        900: 3,
+        500: 2,
+    },
+});
+
+function showImages(number) {
+    const images = document.querySelectorAll(".tile--hidden")
+    console.log(images);
+
+    for (let i = 0; i < number; i++) {
+        console.log(images[i]);
+        images[i].classList.remove('tile--hidden');
+        images[i].classList.add('tile--show');
+        console.log(images[i].classList)
+    }
+
+    
+}
+
+function viewMore() {
+    showImages(9);
+    document.getElementById('view-more').style.display = "none";
+    masonry.reInit();
+}
