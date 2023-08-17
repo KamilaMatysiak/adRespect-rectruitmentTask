84d9d3b31e8d0fc6b0f7a2f70601ac26e74d22c9
xkamikoo <58092037+xkamikoo@users.noreply.github.com>
2023-08-17 16:08:37 +0200

    add popup gallery

--- main.js
+++ main.js
@@ -1,33 +1,67 @@
 var masonry = new Macy({
-    container: '#macy-container',
-    mobileFirst: true,
-    trueOrder: false,
-    waitForImages: false,
-    columns: 1,
-    margin: 43,
-    breakAt: {
-        1600: 4,
-        900: 3,
-        500: 2,
-    },
+  container: "#macy-container",
+  mobileFirst: true,
+  trueOrder: false,
+  waitForImages: false,
+  columns: 1,
+  margin: 43,
+  breakAt: {
+    1600: 4,
+    900: 3,
+    500: 2,
+  },
 });
 
 function showImages(number) {
-    const images = document.querySelectorAll(".tile--hidden")
-    console.log(images);
+  const images = document.querySelectorAll(".tile--hidden");
+  console.log(images);
 
-    for (let i = 0; i < number; i++) {
-        console.log(images[i]);
-        images[i].classList.remove('tile--hidden');
-        images[i].classList.add('tile--show');
-        console.log(images[i].classList)
-    }
-
-    
+  for (let i = 0; i < number; i++) {
+    images[i].classList.remove("tile--hidden");
+    images[i].classList.add("tile--show");
+  }
 }
 
 function viewMore() {
-    showImages(9);
-    document.getElementById('view-more').style.display = "none";
-    masonry.reInit();
+  showImages(9);
+  document.getElementById("view-more").style.display = "none";
+  masonry.reInit();
+}
+
+
+// POP-UP GALLERY //
+
+var imgIndex = 1;
+const allImages = document.querySelectorAll(".gallery-img");
+
+function openImg(id) {
+    showGallery(id);
+}
+
+function showGallery(index) {
+    const gallery = document.querySelector("#popup");
+    gallery.style.display = "flex";
+
+    document.getElementById('popup-img').src = allImages[index-1].src;
+}
+
+function nextImg() {
+    if(imgIndex+1 > allImages.length) imgIndex = 0;
+    imgIndex++;  
+    
+    console.log(imgIndex);
+    showGallery(imgIndex);
+}
+
+function prevImg() {
+    if(imgIndex - 1 < 1) imgIndex = allImages.length+1;
+    imgIndex--;
+
+    console.log(imgIndex);
+    showGallery(imgIndex);
+}
+
+function closeGallery() {
+    const gallery = document.querySelector("#popup");
+    gallery.style.display = "none";
 }
